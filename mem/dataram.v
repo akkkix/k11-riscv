fa87e563dcd7b6acd591706987ba25e546c3c4d1
Taji Akinori <taji@coins.tsukuba.ac.jp>
2021-11-10 02:10:15 +0900

    dataram

--- mem/dataram.v
+++ mem/dataram.v
@@ -1,26 +1,44 @@
 `default_nettype none
 
-module dataram(clk,addr,write,din,dout);
+module dataram(clk,addr,width,write,din,dout);
     parameter WORD = 32;
     parameter ADDR = 32;
     parameter LEN = 65535;
 
     input clk;
     input [ADDR -1:0] addr;
+    input [1:0] width;
     input write;
     input [WORD-1:0] din;
     output [WORD-1:0] dout;
 
-    reg [WORD-1:0] mem_bank [0:LEN];
+    reg [7:0] mem_bank [0:LEN];
     reg [WORD-1:0] o_r;
 
     assign dout = o_r;
 
     always @(posedge clk) begin
         if (write) begin
-            mem_bank[addr] <= din;
+            if(width == 2'b00) begin
+                mem_bank[addr] <= din[7:0];
+            end else if(width == 2'b01) begin
+                mem_bank[addr] <= din[7:0];
+                mem_bank[addr+1] <= din[15:8];
+            end else if(width == 2'b10) begin
+                mem_bank[addr] <= din[7:0];
+                mem_bank[addr+1] <= din[15:8];
+                mem_bank[addr+2] <= din[23:16];
+                mem_bank[addr+3] <= din[32:24];
+            end
+            
         end else begin
-            o_r <= mem_bank[addr];
+            if(width == 2'b00) begin
+                o_r <= {24'd0,mem_bank[addr]};
+            end else if(width == 2'b01) begin
+                o_r <= {16'd0,mem_bank[addr+1],mem_bank[addr]};
+            end else if(width == 2'b10) begin
+                o_r <= {mem_bank[addr+3],mem_bank[addr+2],mem_bank[addr+1],mem_bank[addr]};
+            end
         end
     end
 
